4a883b65a26aabb28b1518f26fdfed8bddee2acf
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-10-03 21:55:17 +0700

    (chore) Change example code to sum problem

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,33 @@ use testgen::problemspec::spec::*;
 use testgen::testspec::spec::*;
 
 struct Spec {
-    t: u64,
-    k: usize,
-    m: usize,
-    n: Vec<u64>,
-    s: Vec<String>,
-    hashed: Vec<u64>,
+    a: Vec<i64>,
+    b: Vec<i64>,
+    sum: Vec<i64>,
 }
 impl ProblemSpec for Spec {
     fn input_format(&self) -> IOFormat {
-        vec![
-            IOElement::Line(vec![LineElement::Scalar(Scalar::UInt(self.t))]),
-            IOElement::EmptyLine,
-            IOElement::RawLine("Hello".to_string()),
-            IOElement::RawLinesBounded(self.s.clone(), self.k),
-            IOElement::LinesBounded(
-                vec![
-                    self.n.iter().map(|&x| Scalar::UInt(x)).collect(),
-                    self.hashed.iter().map(|&x| Scalar::UInt(x)).collect(),
-                ],
-                self.m,
-            ),
-        ]
+        vec![IOElement::LinesUnbounded(vec![
+            self.a.iter().map(|&x| Scalar::Int(x)).collect(),
+            self.b.iter().map(|&x| Scalar::Int(x)).collect(),
+        ])]
     }
 
     fn output_format(&self) -> IOFormat {
-        vec![IOElement::Line(vec![LineElement::Scalar(Scalar::UInt(
-            self.t,
-        ))])]
+        vec![IOElement::LinesUnbounded(vec![self
+            .sum
+            .iter()
+            .map(|&x| Scalar::Int(x))
+            .collect()])]
     }
 
     fn constraints(&self) -> Result<(), Vec<String>> {
         let mut errors = Vec::new();
-        if !(self.t >= 4) {
-            errors.push("t >= 4".to_string());
+        if !self.a.iter().all(|it| *it >= 0) {
+            errors.push("a >= 0".to_string());
         }
-        if !(self.k >= 1) {
-            errors.push("k >= 1".to_string());
+        if !self.b.iter().all(|it| *it >= 0) {
+            errors.push("a >= 0".to_string());
         }
         if errors.is_empty() {
             return Ok(());
@@ -52,20 +42,14 @@ impl TestSpec<Spec> for Spec {
     fn test_cases() -> Vec<Spec> {
         vec![
             Spec {
-                t: 2,
-                k: 2,
-                m: 2,
-                n: vec![1, 2, 3],
-                hashed: vec![4, 5, 6],
-                s: vec!["a".to_string(), "b".to_string(), "c".to_string()],
+                a: vec![1, 2, 3],
+                b: vec![1, 2, 3],
+                sum: vec![],
             },
             Spec {
-                t: 3,
-                k: 3,
-                m: 3,
-                n: vec![1, 2, 3],
-                hashed: vec![4, 5, 6],
-                s: vec!["a".to_string(), "b".to_string(), "c".to_string()],
+                a: vec![4, 5, 6],
+                b: vec![4, 5, 6],
+                sum: vec![],
             },
         ]
     }
