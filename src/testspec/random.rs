bd58f8802a473d105964e241ea570f67500a13c2
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-10-09 22:04:54 +0700

    (test) Test case for range

--- src/testspec/random.rs
+++ src/testspec/random.rs
@@ -29,3 +29,24 @@ impl Random {
         self.rnd.gen_range(from..=to)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_next_to_be_deterministic() {
+        assert_eq!(Random::new(0).next::<i32>(), -852725889);
+        assert_eq!(Random::new(0).next::<u32>(), 3442241407);
+        assert_eq!(Random::new(0).next::<f32>(), 0.80145925);
+        assert_eq!(Random::new(0).next::<f64>(), 0.7311134158637046);
+        assert_eq!(Random::new(0).next::<bool>(), true);
+        assert_eq!(Random::new(0).next::<char>(), '\u{da18a}');
+    }
+
+    #[test]
+    fn test_next_range_returns_number_within_range() {
+        assert!(Random::new(0).next_range(0, 1) <= 1);
+        assert!(Random::new(0).next_range(0, 1) >= 0);
+    }
+}
