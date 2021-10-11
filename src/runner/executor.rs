f02de9e4c5fac11e93f79e93c5735df4532c1ae5
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-10-11 23:13:39 +0700

    (fix) Run stdin write_all in another thread, preventing deadlock https://stackoverflow.com/questions/69528338/why-commands-stdin-write-all-never-terminate

--- src/runner/executor.rs
+++ src/runner/executor.rs
@@ -3,6 +3,7 @@ use std::process::{Command, Stdio};
 
 pub fn execute(solution_command: &str, input: &str) -> String {
     let args = shlex::split(solution_command).unwrap();
+
     let mut cmd = Command::new(&args[0]);
     let mut child = cmd
         .stdin(Stdio::piped())
@@ -10,12 +11,15 @@ pub fn execute(solution_command: &str, input: &str) -> String {
         .args(args.iter().skip(1))
         .spawn()
         .expect("Failed to execute solution");
-    child
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all(input.as_bytes())
-        .unwrap();
+
+    let mut stdin = child.stdin.take().expect("failed to get stdin");
+    let input = input.to_string();
+
+    std::thread::spawn(move || {
+        stdin
+            .write_all(input.as_bytes())
+            .expect("failed to write to stdin");
+    });
 
     let output = child.wait_with_output().unwrap();
     let stdout = output.stdout;
@@ -23,5 +27,6 @@ pub fn execute(solution_command: &str, input: &str) -> String {
     // let stderr = output.stderr;
     // TODO: handle stderr
     // let stderr = String::from_utf8(stderr).unwrap();
+
     stdout
 }
